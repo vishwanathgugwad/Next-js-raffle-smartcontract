a01579739d6d099dfcae3866604405e4fe7e0014
Vishwanath GUgwad <vishwanath.gugwad@gmail.com>
2022-09-18 11:23:34 +0530

    lottery enterance

---
+++ components/Header.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react"
+import { useMoralis  } from "react-moralis"
+
+export default function Header(){
+
+    const {enableWeb3 ,account , isWeb3Enabled,Moralis,deactivateWeb3,isWeb3EnableLoading} = useMoralis()
+
+    useEffect(()=>{
+      if(!isWeb3Enabled && typeof window !== "undefined"){
+        if(window.localStorage.getItem("connected")){
+            enableWeb3()
+        }
+
+      }
+    },[isWeb3Enabled])
+
+    useEffect(()=>{
+        Moralis.onAccountChanged((account) => {
+            console.log( `account changed to account ${account}`)
+            if(account == null){
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log( `null account found `)
+            }
+        })
+    },[])
+    
+    return(<div>
+        {account ? (<div>Connected to {account.slice(0,6)}...{account.slice(account.length - 4)}</div>) : 
+        (<button onClick = {
+            async ()=>{
+                await enableWeb3()
+                if( typeof window !== "undefined"){
+                    window.localStorage.setItem("connected","injected")
+                }
+                } } disabled = {isWeb3EnableLoading}>Connect</button>) } 
+     
+    </div>)
+}
