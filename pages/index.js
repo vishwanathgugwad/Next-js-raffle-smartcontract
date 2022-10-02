f3107a17ba51743056fb3148e685da34f294ed48
Vishwanath GUgwad <vishwanath.gugwad@gmail.com>
2022-10-02 17:37:11 +0530

    image issue fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head"
-import Image from "next/image"
 import styles from "../styles/Home.module.css"
-import Header from "../components/Header"
 import AutoHeader from "../components/AutoHeader"
 import LotteryEnterance from "../components/LotteryEnterance"
 
